c859ed06c53ccbe53e1660144ca22cbee8c44d4b
Ahmed Elazazy <ahmed.elazazy@hotmail.com>
2022-08-16 18:13:47 +0200

    Adding paths & ip

--- index.js
+++ index.js
@@ -1,14 +1,30 @@
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
+const requestIp = require('request-ip')
 
-const appName = process.env.APP_NAME || ''
+const appName = process.env.APP_NAME || '_App_Name_'
+const version = '1.0'
+app.use(requestIp.mw())
 
 app.get('/', (req, res) => {
-	res.send(`App ${appName} is up and running`)
+	res.send(`${appName} is up and running - v${version}`)
+})
+
+app.get('/path1', (req, res) => {
+	res.send(`/path1 - ${appName}`)
+})
+
+app.get('/path2', (req, res) => {
+	res.send(`/path2 - ${appName}`)
+})
+
+app.get('/ip', (req, res) => {
+	const ip = req.clientIp || 'Not detected'
+	res.send(ip)
 })
 
 app.listen(port, () => {
-	console.log(`App ${appName} is up on port ${port}`)
+	console.log(`${appName} is up on port ${port}`)
 })
 
