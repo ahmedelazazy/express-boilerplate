fab37a84c9f8712b9324d3a1e7d268e1e8a19a76
Ahmed Elazazy <ahmed.elazazy@hotmail.com>
2022-08-18 14:07:49 +0200

    v7

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(requestIp.mw())
 app.get('/', (req, res) => {
 	console.log(process.env)
 	const myVar = process.env.MY_VAR
-	res.send(`v6`)
+	res.send(`v7`)
 })
 
 app.get('/path1', (req, res) => {
